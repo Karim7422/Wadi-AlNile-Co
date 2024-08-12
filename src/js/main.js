29c7c1ee9c7e6320c7fdfd044df87cbafbc4b401
Karim7422 <karimhatem7274@gmail.com>
2024-08-13 01:17:16 +0300

    create about section

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,50 @@
+//!-------------------------------------------------Global Variables Start------------------------------------------>
+//!Change SVG Color
+let svgIcons = document.querySelectorAll('#about svg')
+
+//!Navbar Buttons
+const openBTN = document.getElementById('nav-open')
+const closeBTN = document.getElementById('nav-close')
+const navList = document.getElementById('nav-list')
 //Toggle Dark And Light Mode 
 let mode = localStorage.getItem('Current Mode');
 const windowsMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+const btnToggler = document.getElementsByClassName('togglerBtn');
+const toggleModeArr = Array.from(btnToggler);
+//!-------------------------------------------------Global Variables End------------------------------------------>
+document.addEventListener('DOMContentLoaded', () => {
+    changeSVGColor()
+    closeBTN.classList.add('hidden')
+})
+function changeSVGColor() {
+    if (mode == 'light') {
+        svgIcons.forEach((el) => {
+            el.setAttribute('fill', "#000")
+        })
+    } else if (mode == 'dark') {
+        svgIcons.forEach((el) => {
+            el.setAttribute('fill', "#fff")
+        })
+    }
+}
+//Toggle Dark And Light Mode 
 if (mode === null) {
     mode = windowsMode ? 'dark' : 'light';
 }
 localStorage.setItem('Current Mode', mode);
 document.documentElement.classList.add(mode);
-
-const btnToggler = document.getElementsByClassName('togglerBtn');
-const toggleModeArr = Array.from(btnToggler);
 toggleModeArr.map((el) => {
     el.addEventListener('click', () => {
         mode = mode === 'light' ? 'dark' : 'light';
+        changeSVGColor()
         localStorage.setItem('Current Mode', mode);
         document.documentElement.classList.toggle('dark');
     });
 });
 
-// navbar buttons toggler 
-const openBTN = document.getElementById('nav-open')
-const closeBTN = document.getElementById('nav-close')
-const navList = document.getElementById('nav-list')
 
-document.addEventListener('DOMContentLoaded', () => {
-    closeBTN.classList.add('hidden')
-})
+
+
 openBTN.addEventListener('click', () => {
     navList.classList.add('h-screen')
     closeBTN.classList.remove('hidden')
